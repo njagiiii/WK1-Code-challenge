e552b1428beddcf4b999a5fc6f8c21b32c3aa43f
njagiiii <grace.makena@student.moringaschool.com>
2023-06-16 15:08:07 -0400

    completed code challenge

---
+++ main.js
@@ -0,0 +1,25 @@
+
+//define a function
+
+function car(){
+   return function(speed){  //return the main function from this function
+        const limit = 70;  //initialize the limit argument
+        const kilometers = 5; // initialize kilometer argument
+
+        if (speed <= limit){    
+            return "OK"        // give a condition that checks the speed
+        }
+        else{
+            const result=(speed - limit)/kilometers;
+                                                          //if the speed is above limit minus then divide
+            if(result <=12 ){
+                return `points: ${result}`;
+            }
+            else{
+                return "license Suspended";   //check the points and output based by the condition 
+            }
+        }
+   }
+}
+// call the main function and the return function
+console.log(car()(10));
